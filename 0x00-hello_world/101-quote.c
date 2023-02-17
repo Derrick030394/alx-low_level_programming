49c17bbd166ae9311c9213c836cabf557c3d5227
Thembinkosi <derrickthembie@gmail.com>
2023-02-17 01:00:35 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints text
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
+return (0);
+}
