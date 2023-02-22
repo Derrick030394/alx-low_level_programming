ae71affd1314c7570ae6d4fd19db445c64b2b1e4
Thembinkosi <derrickthembie@gmail.com>
2023-02-22 01:41:15 -0800

    Alphabet times 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,16 @@ void print_alphabet_x10(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
+	int a;
+
+	a = 0;
+	while (a <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		a++;
+		_putchar('\n');
+	}
 }
