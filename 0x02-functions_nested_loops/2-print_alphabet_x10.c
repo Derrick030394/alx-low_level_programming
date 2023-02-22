dc565d3ad9c39379a8f810acdf27232c1f0a39a2
Thembinkosi <derrickthembie@gmail.com>
2023-02-22 00:24:13 -0800

    Alphabet times 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,11 @@
  #include "main.h"
 
 /**
- * print_alphabet - print all alphabet in lowercase 10 times
+ * print_alphabet_x10 - print all alphabet in lowercase
+ *
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char letter;
 
